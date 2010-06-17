144fff028ae99770a14f4a8888483dcbfd8a4d6a
Sam Lown <me@samlown.com>
2010-06-17 02:39:55 +0200

    Raising version to match history

--- lib/couchrest/extended_document.rb
+++ lib/couchrest/extended_document.rb
@@ -8,7 +8,7 @@ module CouchRest
   # Same as CouchRest::Document but with properties and validations
   class ExtendedDocument < Document
 
-    VERSION = "1.0.0.beta5"
+    VERSION = "1.0.0.beta6"
 
     include CouchRest::Mixins::Callbacks
     include CouchRest::Mixins::DocumentQueries    
