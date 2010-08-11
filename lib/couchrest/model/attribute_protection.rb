aac6b80d2671c8b2767a1bb7d550d49ed799d295
Will Leinweber <will@bitfission.com>
2010-08-11 17:41:32 -0500

    Allow mixing of protected and accessible properties.
    Any unspecified properties are now assumed to be protected by default

--- lib/couchrest/model/attribute_protection.rb
+++ lib/couchrest/model/attribute_protection.rb
@@ -1,25 +1,31 @@
 module CouchRest
   module Model
     module AttributeProtection
-      # Attribute protection from mass assignment to CouchRest properties
-      # 
+      # Attribute protection from mass assignment to CouchRest::Model properties
+      #
       # Protected methods will be removed from
-      #  * new 
+      #  * new
       #  * update_attributes
       #  * upate_attributes_without_saving
       #  * attributes=
-      #  
-      # There are two modes of protection
-      #  1) Declare accessible poperties, assume all the rest are protected 
-      #    property :name, :accessible => true
-      #    property :admin                     # this will be automatically protected
       #
-      #  2) Declare protected properties, assume all the rest are accessible
-      #    property :name                      # this will not be protected
+      # There are two modes of protection
+      #  1) Declare accessible poperties, and assume all unspecified properties are protected
+      #    property :name,  :accessible => true
+      #    property :admin                      # this will be automatically protected
+      #
+      #  2) Declare protected properties, and assume all unspecified properties are accessible
+      #    property :name                       # this will not be protected
       #    property :admin, :protected => true
       #
-      # Note: you cannot set both flags in a single class
-      
+      #  3) Mix and match, and assume all unspecified properties are protected.
+      #    property :name,  :accessible => true
+      #    property :admin, :protected  => true
+      #    property :phone                      # this will be automatically protected
+      #
+      #  Note: the timestamps! method protectes the created_at and updated_at properties
+
+
       def self.included(base)
         base.extend(ClassMethods)
       end
@@ -56,18 +62,13 @@ module CouchRest
       private
 
       def properties_to_remove_from_mass_assignment
-        has_protected = !protected_properties.empty?
-        has_accessible = !accessible_properties.empty?
+        to_remove = protected_properties
 
-        if !has_protected && !has_accessible
-          []
-        elsif has_protected && !has_accessible
-          protected_properties
-        elsif has_accessible && !has_protected
-          properties.reject { |prop| prop.options[:accessible] }
-        else
-          raise "Set either :accessible or :protected for #{self.class}, but not both"
+        unless accessible_properties.empty?
+          to_remove += properties.reject { |prop| prop.options[:accessible] }
         end
+
+        to_remove
       end
     end
   end
--- spec/couchrest/attribute_protection_spec.rb
+++ spec/couchrest/attribute_protection_spec.rb
@@ -23,13 +23,13 @@ describe "Model Attributes" do
       user.name.should == "will"
       user.phone.should == "555-5555"
     end
-    
+
     it "should recreate from the database properly" do
       user = NoProtection.new
       user.name = "will"
       user.phone = "555-5555"
       user.save!
-      
+
       user = NoProtection.get(user.id)
       user.name.should == "will"
       user.phone.should == "555-5555"
@@ -50,7 +50,7 @@ describe "Model Attributes" do
     end
 
     it "should protect non-accessible properties set through new" do
-      user = WithAccessible.new(:name => "will", :admin => true) 
+      user = WithAccessible.new(:name => "will", :admin => true)
 
       user.name.should == "will"
       user.admin.should == false
@@ -79,7 +79,7 @@ describe "Model Attributes" do
     end
 
     it "should protect non-accessible properties set through new" do
-      user = WithProtected.new(:name => "will", :admin => true) 
+      user = WithProtected.new(:name => "will", :admin => true)
 
       user.name.should == "will"
       user.admin.should == false
@@ -94,15 +94,22 @@ describe "Model Attributes" do
     end
   end
 
-  describe "protected flag" do
-    class WithBoth < CouchRest::Model::Base
+  describe "Model Base", "mixing protected and accessible flags" do
+    class WithBothAndUnspecified < CouchRest::Model::Base
       use_database TEST_SERVER.default_database
       property :name, :accessible => true
       property :admin, :default => false, :protected => true
+      property :phone, :default => 'unset phone number'
     end
 
-    it "should raise an error when both are set" do
-      lambda { WithBoth.new }.should raise_error 
+    it { expect { WithBothAndUnspecified.new }.to_not raise_error }
+
+    it 'should assume that any unspecified property is protected by default' do
+      user = WithBothAndUnspecified.new(:name => 'will', :admin => true, :phone => '555-1234')
+
+      user.name.should == 'will'
+      user.admin.should == false
+      user.phone.should == 'unset phone number'
     end
   end
 
@@ -113,7 +120,7 @@ describe "Model Attributes" do
       property :admin, :default => false, :protected => true
       view_by :name
     end
-    
+
     before(:each) do
       @user = WithProtected.new
       @user.name = "will"
@@ -128,12 +135,12 @@ describe "Model Attributes" do
 
     it "Base#get should not strip protected attributes" do
       reloaded = WithProtected.get( @user.id )
-      verify_attrs reloaded 
+      verify_attrs reloaded
     end
 
     it "Base#get! should not strip protected attributes" do
       reloaded = WithProtected.get!( @user.id )
-      verify_attrs reloaded 
+      verify_attrs reloaded
     end
 
     it "Base#all should not strip protected attributes" do
@@ -141,13 +148,13 @@ describe "Model Attributes" do
       docs = WithProtected.all(:key => @user.id)
       docs.size.should == 1
       reloaded = docs.first
-      verify_attrs reloaded 
+      verify_attrs reloaded
     end
 
     it "views should not strip protected attributes" do
       docs = WithProtected.by_name(:startkey => "will", :endkey => "will")
       reloaded = docs.first
-      verify_attrs reloaded 
+      verify_attrs reloaded
     end
   end
 end
