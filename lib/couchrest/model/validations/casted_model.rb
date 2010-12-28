731338ed6c352aa4e4b2d3938e6d8d885adf1a1b
Greg Sterndale <gsterndale@gmail.com>
2010-12-28 15:24:59 -0500

    Do not specify options[:message] for ActiveModel::Errors#add if it's nil

--- lib/couchrest/model/validations/casted_model.rb
+++ lib/couchrest/model/validations/casted_model.rb
@@ -6,7 +6,9 @@ module CouchRest
         def validate_each(document, attribute, value)
           values = value.is_a?(Array) ? value : [value]
           return if values.collect {|doc| doc.nil? || doc.valid? }.all?
-          document.errors.add(attribute, :invalid, :message => options[:message], :value => value)
+          error_options = { :value => value }
+          error_options[:message] = options[:message] if options[:message]
+          document.errors.add(attribute, :invalid, error_options)
         end
       end
     end
