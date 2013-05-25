7d6370c44da9649222886bdea41652a546b39550
Sam Lown <me@samlown.com>
2013-05-25 16:00:50 +0200

    Started on specs for design migrations

---
+++ spec/unit/utils/migrate_spec.rb
@@ -0,0 +1,30 @@
+
+require 'spec_helper'
+
+describe CouchRest::Model::Utils::Migrate do
+
+  before :each do
+    @module = CouchRest::Model::Utils::Migrate
+  end
+
+  describe "#load_all_models" do
+    it "should not do anything if Rails is not available" do
+      @module.load_all_models
+    end
+    it "should detect if Rails is available and require models" do
+      Rails = mock()
+      Rails.stub!(:root).and_return("")
+      Dir.should_receive(:[]).with("app/models/**/*.rb").and_return(['failed_require'])
+      # we can't mock require, so just expect an error
+      expect {
+        @module.load_all_models
+      }.to raise_error(LoadError)
+    end
+  end
+
+  describe "#all_models" do
+    
+  end
+
+
+end
